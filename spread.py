c3819069cd9bb897a3283fdbb3184018592b9e76
katerynak <konotopska.k@gmail.com>
2019-10-19 12:38:11 +0200

    fixed nondeterminism

--- spread.py
+++ spread.py
@@ -9,17 +9,22 @@ import math
 	Suits (un)directed graphs. 
 	Assumes the edges point OUT of the influencer, e.g., if A->B or A-B, then "A influences B".
 """
-def IC_model(G, a, p):              # a: the set of initial active nodes
-	                                # p: the system-wide probability of influence on an edge, in [0,1]
-	A = set(a)                      # A: the set of active nodes, initially a
-	B = set(a)                      # B: the set of nodes activated in the last completed iteration
+
+
+def IC_model(G, a, p, random_generator=None):  # a: the set of initial active nodes
+	# p: the system-wide probability of influence on an edge, in [0,1]
+
+	if random_generator is None:
+		random_generator = random.Random()
+	A = set(a)  # A: the set of active nodes, initially a
+	B = set(a)  # B: the set of nodes activated in the last completed iteration
 	converged = False
 
 	while not converged:
 		nextB = set()
 		for n in B:
-			for m in set(G.neighbors(n)) - A: # G.neighbors follows A-B and A->B (successor) edges
-				prob = random.random() # in the range [0.0, 1.0)
+			for m in set(G.neighbors(n)) - A:  # G.neighbors follows A-B and A->B (successor) edges
+				prob = random_generator.random()  # in the range [0.0, 1.0)
 				if prob <= p:
 					nextB.add(m)
 		B = set(nextB)
@@ -29,10 +34,15 @@ def IC_model(G, a, p):              # a: the set of initial active nodes
 
 	return len(A)
 
-def WC_model(G, a):                 # a: the set of initial active nodes
-                                    # each edge from node u to v is assigned probability 1/in-degree(v) of activating v
-	A = set(a)                      # A: the set of active nodes, initially a
-	B = set(a)                      # B: the set of nodes activated in the last completed iteration
+
+def WC_model(G, a, random_generator):  # a: the set of initial active nodes
+	# each edge from node u to v is assigned probability 1/in-degree(v) of activating v
+
+	if random_generator is None:
+		random_generator = random.Random()
+
+	A = set(a)  # A: the set of active nodes, initially a
+	B = set(a)  # B: the set of nodes activated in the last completed iteration
 	converged = False
 
 	if nx.is_directed(G):
@@ -44,8 +54,8 @@ def WC_model(G, a):                 # a: the set of initial active nodes
 		nextB = set()
 		for n in B:
 			for m in set(G.neighbors(n)) - A:
-				prob = random.random() # in the range [0.0, 1.0)
-				p = 1.0/my_degree_function(m)
+				prob = random_generator.random()  # in the range [0.0, 1.0)
+				p = 1.0 / my_degree_function(m)
 				if prob <= p:
 					nextB.add(m)
 		B = set(nextB)
@@ -55,23 +65,31 @@ def WC_model(G, a):                 # a: the set of initial active nodes
 
 	return len(A)
 
+
 """ Evaluates a given seed set A, simulated "no_simulations" times.
 	Returns a tuple: (the mean, the stdev).
 """
-def MonteCarlo_simulation(G, A, p, no_simulations, model):
+
+
+def MonteCarlo_simulation(G, A, p, no_simulations, model, random_seed=None):
+	if random_seed is not None:
+		random_generator = random.Random(random_seed)
+	else:
+		random_generator = random.Random()
+
 	results = []
 
 	if model == 'WC':
 		for i in range(no_simulations):
-			results.append(WC_model(G, A))
+			results.append(WC_model(G, A, random_generator))
 	elif model == 'IC':
 		for i in range(no_simulations):
-			results.append(IC_model(G, A, p))
+			results.append(IC_model(G, A, p, random_generator))
 
 	return (numpy.mean(results), numpy.std(results))
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
 	G = nx.path_graph(100)
 	print(nx.classes.function.info(G))
-	print(MonteCarlo_simulation(G, [0, 2, 4, 6, 8, 10], 0.1, 100, 'IC'))
+	print(MonteCarlo_simulation(G, [0, 2, 4, 6, 8, 10], 0.1, 100, 'IC', 0))
