fb4228074d04f4dbe03a553b5b7003b084410c14
katerynak <konotopska.k@gmail.com>
2019-10-25 15:27:25 +0200

    spread comparison in params

--- experiments/spread_functions_comparisons/create_in_files.py
+++ experiments/spread_functions_comparisons/create_in_files.py
@@ -9,7 +9,7 @@ def default_values_dict():
 	args["k"] = 5
 	args["p"] = 0.3
 	# args["spread_function"] =
-	args["no_simulations"] = 100
+	args["no_simulations"] = 10
 	args["max_hop"] = 2
 	# args["model"] =
 	args["population_size"] = 16
@@ -17,7 +17,6 @@ def default_values_dict():
 	args["random_seed"] = 42
 	args["max_generations"] = 10
 	args["n_parallel"] = 4
-	# args["multithread"] = False
 	args["g_nodes"] = 1000
 	args["g_new_edges"] = 3
 	args["g_type"] = 'barabasi_albert'
@@ -26,45 +25,50 @@ def default_values_dict():
 	# args["out_file"] =
 
 	# args["out_dir"] =
-	args["print_mc_best"] = True
 
 	return make_dict_read_only(args)
 
 
-n_repetitions = 5
+n_repetitions = 1
+random_seeds = range(5)
+
 script = "evolutionaryalgorithm.py"
 
 
-# K = [1, 2, 5, 7, 10]
-# P = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+K = [1, 2, 5, 7, 10]
+P = [0.1, 0.3, 0.5, 0.7, 0.9]
+Pop_size = [8, 16, 32, 64, 128]
+Max_gen = [5, 10, 15, 20, 30]
+MC_simulations = [10, 50, 100, 500, 1000]
 
-K = [1, 2]
-P = [0.1, 0.2]
 
-
-vars2compare = ['p', 'k']
-values = [P, K]
+vars2compare = ['p', 'k', 'population_size', 'max_generations', 'no_simulatins']
+values = [P, K, Pop_size, Max_gen, MC_simulations]
 spread_functions = ["monte_carlo", "monte_carlo_max_hop", "two_hop"]
 models = ["IC", "WC"]
 for i, var in enumerate(vars2compare):
 	for value in values[i]:
 		for spr_func in spread_functions:
 			for model in models:
-				args = default_values_dict().get_copy()
-				args[var] = value
-				args["spread_function"] = spr_func
-				args["model"] = model
-				args["out_name"] = "{}_{}_".format(var, value)
+				for seed in random_seeds:
+					args = default_values_dict().get_copy()
+					args[var] = value
+					if var == 'population_size':
+						args['offspring_size'] = value
+					args["spread_function"] = spr_func
+					args["model"] = model
+					args["out_name"] = "{}_{}_".format(var, value)
+					args["random_seed"] = seed
 
-				# write out_file
-				in_dir = "./in/" + var + "/" + model + "/" + "{}".format(value) + "/" + spr_func
-				if not os.path.exists(in_dir):
-					os.makedirs(in_dir)
+					# write out_file
+					in_dir = "./in/" + var + "/" + model + "/" + spr_func + "/" + "{}".format(value)
+					if not os.path.exists(in_dir):
+						os.makedirs(in_dir)
 
-				data = dict()
-				data["script"] = script
-				data["n_repetitions"] = n_repetitions
-				data["script_args"] = args
+					data = dict()
+					data["script"] = script
+					data["n_repetitions"] = n_repetitions
+					data["script_args"] = args
 
-				with open(in_dir + '/' + 'exp_in.json', 'w') as outfile:
-					json.dump(data, outfile, indent=4)
+					with open(in_dir + '/' + 'seed_{}_'.format(seed) + 'exp_in.json', 'w') as outfile:
+						json.dump(data, outfile, indent=4)
