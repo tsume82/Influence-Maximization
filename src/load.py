81d6581f3c6652eae5c6951480f5dfb5d4c1077a
katerynak <konotopska.k@gmail.com>
2019-11-04 14:11:57 +0100

    refactoring

--- src/load.py
+++ src/load.py
@@ -3,19 +3,22 @@ import networkx as nx
 
 """ Graph loading """
 
-def read_graph(filename, directed=False, nodetype=int):
 
+def read_graph(filename, directed=False, nodetype=int):
 	graph_class = nx.DiGraph() if directed else nx.Graph()
 	G = nx.read_edgelist(filename, create_using=graph_class, nodetype=nodetype, data=False)
-
-	msg = ' '.join(["Read from file", filename, "the", "directed" if directed else "undirected", "graph\n",
-		nx.classes.function.info(G)])
-	logging.info(msg)
-
 	return G
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
 	logger = logging.getLogger('')
 	logger.setLevel(logging.DEBUG)
-	read_graph("graphs/facebook_combined_undirected.txt")
+
+	filename = "../graphs/wiki-Vote.txt"
+	directed = True
+	G = read_graph(filename, directed=directed)
+
+	msg = ' '.join(["Read from file", filename, "the", "directed" if directed else "undirected", "graph\n",
+					nx.classes.function.info(G)])
+
+	logging.info(msg)
