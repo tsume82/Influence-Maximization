ed5ee8480f48ae5f2b033be3d91247e8d9322238
katerynak <konotopska.k@gmail.com>
2019-11-05 15:18:51 +0100

    minor changes

--- tests/two_hop_spread_test.py
+++ tests/two_hop_spread_test.py
@@ -17,30 +17,30 @@ class TestTwoHopSpread(unittest.TestCase):
 		self.G2_WC = add_weights_WC(G2)
 
 	def test_one_hop(self):
-		one_hop_spr = one_hop_spread_single_node(self.G_IC, 4)
+		one_hop_spr = one_hop_spread_single_node(self.G_IC, 4, 0.1, "IC")
 		# eliminate error due to machine approximation
 		one_hop_spr = round(one_hop_spr, 5)
 		self.assertEqual(one_hop_spr, 1.3)
 
-		one_hop_spr = one_hop_spread_single_node(self.G_WC, 4)
+		one_hop_spr = one_hop_spread_single_node(self.G_WC, 4, 0.1, "WC")
 		one_hop_spr = round(one_hop_spr, 5)
 		self.assertEqual(one_hop_spr, 2.25)
 
 	def test_two_hop(self):
 		S = [4, 1]
-		two_hop_spr = two_hop_spread(self.G_IC, S)
+		two_hop_spr = two_hop_spread(self.G_IC, S, 0.1, "IC")
 		two_hop_spr = round(two_hop_spr, 5)
 		self.assertEqual(two_hop_spr, 2.46)
 
-		two_hop_spr = two_hop_spread(self.G_WC, S)
+		two_hop_spr = two_hop_spread(self.G_WC, S, 0.1, "WC")
 		two_hop_spr = round(two_hop_spr, 5)
 		self.assertEqual(two_hop_spr, 4.25)
 
 		S2 = [0, 2, 5]
-		two_hop_spr = two_hop_spread(self.G2_IC, S2)
+		two_hop_spr = two_hop_spread(self.G2_IC, S2, 0.1, "IC")
 		two_hop_spr = round(two_hop_spr, 5)
 		self.assertEqual(two_hop_spr, 3.69)
 
-		two_hop_spr = two_hop_spread(self.G2_WC, S2)
+		two_hop_spr = two_hop_spread(self.G2_WC, S2, 0.1, "WC")
 		two_hop_spr = round(two_hop_spr, 5)
 		self.assertEqual(two_hop_spr, 5.95833)
