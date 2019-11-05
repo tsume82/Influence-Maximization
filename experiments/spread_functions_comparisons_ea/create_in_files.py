18ccf650d76486b19c38fda5b67f64f2d2dc7e21
katerynak <konotopska.k@gmail.com>
2019-11-05 16:39:15 +0100

    wiki & amazon experiment setting

--- experiments/spread_functions_comparisons_ea/create_in_files.py
+++ experiments/spread_functions_comparisons_ea/create_in_files.py
@@ -34,18 +34,57 @@ random_seeds = range(5)
 
 script = "evolutionaryalgorithm.py"
 
-K = [1, 2, 5, 7, 10]
+# K = [1, 2, 5, 7, 10]
+K = range(1, 22, 2)
 P = [0.1, 0.3, 0.5, 0.7, 0.9]
 Pop_size = [8, 16, 32, 64, 128]
 Max_gen = [5, 10, 15, 20, 30]
 MC_simulations = [10, 50, 100, 500, 1000]
 
 
-vars2compare = ['p', 'k', 'population_size', 'max_generations', 'no_simulations']
-values = [P, K, Pop_size, Max_gen, MC_simulations]
+vars2compare = ['k', 'population_size', 'max_generations', 'no_simulations']
+values = [K, Pop_size, Max_gen, MC_simulations]
 spread_functions = ["monte_carlo", "monte_carlo_max_hop", "two_hop"]
 models = ["IC", "WC"]
-graph_types = ["barabasi_albert", "gaussian_random_partition"]
+# graph_types = ["barabasi_albert", "gaussian_random_partition"]
+graph_types = ["wiki", "amazon"]
+for i, var in enumerate(vars2compare):
+	for value in values[i]:
+		for spr_func in spread_functions:
+			for model in models:
+				for seed in random_seeds:
+					for graph_t in graph_types:
+						args = default_values_dict().get_copy()
+						args[var] = value
+						if var == 'population_size':
+							args['offspring_size'] = value
+						args["spread_function"] = spr_func
+						args["model"] = model
+						args["out_name"] = "{}_{}_".format(var, value)
+						args["random_seed"] = seed
+						args["g_type"] = graph_t
+
+						# write out_file
+						in_dir = "./in/" + graph_t + "/" + var + "/" + model + "/" + spr_func + "/" + "{}".format(value)
+						if not os.path.exists(in_dir):
+							os.makedirs(in_dir)
+
+						data = dict()
+						data["script"] = script
+						data["n_repetitions"] = n_repetitions
+						data["script_args"] = args
+
+						with open(in_dir + '/' + 'seed_{}_'.format(seed) + 'exp_in.json', 'w') as outfile:
+							json.dump(data, outfile, indent=4)
+
+# IC only
+
+vars2compare = ['p']
+values = [P]
+spread_functions = ["monte_carlo", "monte_carlo_max_hop", "two_hop"]
+models = ["IC"]
+# graph_types = ["barabasi_albert", "gaussian_random_partition"]
+graph_types = ["wiki", "amazon"]
 for i, var in enumerate(vars2compare):
 	for value in values[i]:
 		for spr_func in spread_functions:
