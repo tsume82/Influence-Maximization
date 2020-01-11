ecd602247f6b68a0fd48dddd118073501e8fc6f0
katerynak <konotopska.k@gmail.com>
2020-01-11 22:16:16 +0100

    subpopulation generator

--- src/ea/generators.py
+++ src/ea/generators.py
@@ -9,3 +9,19 @@ def generator(random, args):
 	"""
 	#TODO: sostituire pool di nodi da bounder, dove i nodi nel caso vengono filtrati
 	return random.sample(args["G"].nodes(), args["k"])
+
+
+def subpopulation_generator(random, args):
+	"""
+	for each dimension selects node from one cell
+	:param random:
+	:param args:
+	:return:
+	"""
+	individual = []
+	voronoi_cells = args["voronoi_cells"]
+	for i in range(args["k"]):
+		nodes = voronoi_cells[list(voronoi_cells.keys())[i]]
+		individual.append(random.sample(nodes, 1)[0])
+
+	return individual
