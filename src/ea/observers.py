4e5d7b2ea19c2334da3e63893a6acfefd235935a
katerynak <konotopska.k@gmail.com>
2020-03-04 23:34:30 +0100

    dynamic population

--- src/ea/observers.py
+++ src/ea/observers.py
@@ -1,10 +1,12 @@
 from utils import diversity, individuals_diversity
 import os
+import inspyred.ec
+from ea.generators import generator_new_nodes
 
 
 def ea_observer1(population, num_generations, num_evaluations, args):
 	"""
-	debug info, printing to stout some generational info
+	debug info, printing to stdout some generational info
 	"""
 	# to access to evolutionary computation stuff
 	div = diversity(population)
@@ -14,7 +16,6 @@ def ea_observer1(population, num_generations, num_evaluations, args):
 		args["local_mutation_rate"] = div
 	ind_div = individuals_diversity(population)
 	print("generation {}: individuals diversity {}".format(num_generations, ind_div))
-	print("Mutations reward {}".format(args["mab"].sums_of_reward))
 
 	# check for repetitions
 	for ind in population:
@@ -22,7 +23,26 @@ def ea_observer1(population, num_generations, num_evaluations, args):
 			raise NameError("Nodes repetition inside an individual")
 	# reset offspring fitnesses
 	args["offspring_fitness"] = {}
-	args["mab"].exploration_weight = 1/(num_generations+1)**(1.5)
+	# exploration weight exponential decay
+	#TODO: use a logarithm?
+	if args["mab"] is not None:
+		args["mab"].exploration_weight = 1/(num_generations+1)**(3)
+		print("Mab selections: {}".format(args["mab"].n_selections))
+	print("Population size: {}".format(len(population)))
+
+	if args["dynamic_population"]:
+		if len(population) < 100:
+			if "improvement" in args.keys():
+				if sum(args["improvement"]) == 0:
+					new_individuals = min(int(1/div), 10)
+					for _ in range(new_individuals):
+						# candidate = args["_ec"].generator(args["prng"], args)
+						candidate = generator_new_nodes(args["prng"], args)
+						args["_ec"].population.append(inspyred.ec.Individual(candidate=candidate))
+						args["_ec"].population[-1].fitness = args["fitness_function"](A=candidate)[0]
+					args["num_selected"] = len(args["_ec"].population)
+			else:
+				args["improvement"] = [0]*3
 	return
 
 
@@ -48,6 +68,10 @@ def ea_observer2(population, num_generations, num_evaluations, args):
 
 	sf.write(",{},".format(diversity(population)))
 	sf.write("{},".format(generation_stats["improvement"]))
-	sf.write("{}\n".format(args["mab"].n_selections))
+	if args["mab"] is not None:
+		sf.write("{}\n".format(args["mab"].n_selections))
+	if args["dynamic_population"]:
+		args["improvement"].pop(0)
+		args["improvement"].append(generation_stats["improvement"])
 
 	return
