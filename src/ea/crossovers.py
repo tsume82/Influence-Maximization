68eb1cde99d7b22d38f8c2ff14c65b71157386de
katerynak <konotopska.k@gmail.com>
2020-03-04 23:29:45 +0100

    guarantee on having two different individuals

--- src/ea/crossovers.py
+++ src/ea/crossovers.py
@@ -1,4 +1,5 @@
 import inspyred
+from ea.mutators import ea_global_random_mutation
 
 
 @inspyred.ec.variators.crossover
@@ -7,11 +8,23 @@ def ea_one_point_crossover(prng, candidate1, candidate2, args):
 	applies 1-point crossover by avoiding repetitions
 	"""
 	# see common elements
-	common = list(set(candidate1).intersection(candidate2))
+	common = list(set(candidate1).intersection(set(candidate2)))
+	max_trials = 5
+	while (len(candidate1) - len(common)) < 2 and max_trials > 0:
+		# while candidates have less then 2 nodes in common crossover would not produce any new candidates,
+		# so mutation is forced
+		if (len(candidate1) - len(common) == 1):
+			candidate1 = ea_global_random_mutation(prng, candidate1, args)
+			candidate2 = ea_global_random_mutation(prng, candidate2, args)
+		elif (len(candidate1)==len(common)):
+			for _ in range(2):
+				candidate1 = ea_global_random_mutation(prng, candidate1, args)
+				candidate2 = ea_global_random_mutation(prng, candidate2, args)
+		max_trials -= 1
+		common = list(set(candidate1).intersection(set(candidate2)))
 
-	# if two candidates are same
-	if len(common) == len(candidate1):
-		return [candidate1]
+	if max_trials==0:
+		return [candidate2, candidate1]
 
 	candidate1_to_swap = candidate1.copy()
 	candidate2_to_swap = candidate2.copy()
@@ -28,7 +41,8 @@ def ea_one_point_crossover(prng, candidate1, candidate2, args):
 		c2_common[idx2] = c
 
 	# choose swap position
-	swap_idx = prng.randint(0, len(candidate1_to_swap) - 1)
+
+	swap_idx = prng.randint(1, len(candidate1_to_swap) - 1)
 	swap = candidate1_to_swap[swap_idx:]
 	candidate1_to_swap[swap_idx:] = candidate2_to_swap[swap_idx:]
 	candidate2_to_swap[swap_idx:] = swap
@@ -37,5 +51,9 @@ def ea_one_point_crossover(prng, candidate1, candidate2, args):
 		candidate1_to_swap.insert(idx, c)
 	for (idx, c) in c2_common.items():
 		candidate2_to_swap.insert(idx, c)
+
+	# if set(candidate1_to_swap) == set(candidate2) or set(candidate1_to_swap)==set(candidate1):
+	# 	print(candidate1_to_swap, candidate2_to_swap)
+	# 	exit(0)
 
 	return [candidate1_to_swap, candidate2_to_swap]
