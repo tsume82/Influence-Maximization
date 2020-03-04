3ee495fa0fefec871cdf5b233a406c73dc967370
katerynak <konotopska.k@gmail.com>
2020-03-04 23:31:52 +0100

    new individual with a percentage of new nodes

--- src/ea/generators.py
+++ src/ea/generators.py
@@ -12,6 +12,48 @@ def generator(random, args):
 	return random.sample(args["nodes"], args["k"])
 
 
+def generator_new_nodes(random, args, new_nodes_percentage=0.9):
+	"""
+	generator which tries to generate an individual having a specified percentage of nodes not already present
+	in the population
+	:param random:
+	:param args:
+	:param new_nodes_percentage:
+	:return:
+	"""
+	new_nodes = int(args["k"]*new_nodes_percentage)
+	nodes = args["nodes"].copy()
+	population = args["_ec"].population
+	pop_nodes = []
+
+	# collect population nodes
+	for individual in population:
+		for n in individual.candidate:
+			if n not in pop_nodes:
+				pop_nodes.append(n)
+
+	# separate new nodes
+	for node in pop_nodes:
+		if node in nodes:
+			nodes.remove(node)
+
+	if len(nodes) < new_nodes:
+		first_part = nodes
+		second_part = random.sample(args["nodes"], args["k"] - len(nodes))
+		new_ind = first_part + second_part
+	else:
+		# new nodes
+		first_part = random.sample(nodes, new_nodes)
+		# population nodes
+		second_part = random.sample(pop_nodes, args["k"]-new_nodes)
+		new_ind = first_part + second_part
+
+	# shuffle new and already present nodes
+	random.shuffle(new_ind)
+
+	return new_ind
+
+
 def subpopulation_generator(random, args):
 	"""
 	for each dimension selects node from one cell
