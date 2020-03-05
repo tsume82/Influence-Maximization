ee63aba632791e9e8b2fcae8b0b64c4fc4baa6fd
katerynak <konotopska.k@gmail.com>
2020-03-05 10:48:30 +0100

    dynamic population

--- src/ea/observers.py
+++ src/ea/observers.py
@@ -24,25 +24,40 @@ def ea_observer1(population, num_generations, num_evaluations, args):
 	# reset offspring fitnesses
 	args["offspring_fitness"] = {}
 	# exploration weight exponential decay
-	#TODO: use a logarithm?
 	if args["mab"] is not None:
 		args["mab"].exploration_weight = 1/(num_generations+1)**(3)
 		print("Mab selections: {}".format(args["mab"].n_selections))
 	print("Population size: {}".format(len(population)))
 
+	prev_best = args["prev_population_best"]
+	current_best = max(population).fitness
+
+	improvement = (current_best - prev_best) / prev_best
+
 	if args["dynamic_population"]:
+		if "improvement" in args.keys():
+			args["improvement"].pop(0)
+			args["improvement"].append(improvement)
+		else:
+			args["improvement"] = [0] * 3
 		if len(population) < 100:
-			if "improvement" in args.keys():
-				if sum(args["improvement"]) == 0:
-					new_individuals = min(int(1/div), 10)
+				if sum(args["improvement"]) == 0 and num_generations>2:
+					# new_individuals = min(int(1/div), 10)
+					new_individuals = 1
 					for _ in range(new_individuals):
 						# candidate = args["_ec"].generator(args["prng"], args)
 						candidate = generator_new_nodes(args["prng"], args)
 						args["_ec"].population.append(inspyred.ec.Individual(candidate=candidate))
 						args["_ec"].population[-1].fitness = args["fitness_function"](A=candidate)[0]
 					args["num_selected"] = len(args["_ec"].population)
-			else:
-				args["improvement"] = [0]*3
+				elif len(population) > 2 and improvement>0:
+					min_fit_ind = args["_ec"].population[0]
+					for ind in args["_ec"].population:
+						if ind.fitness < min_fit_ind.fitness:
+							min_fit_ind = ind
+					args["_ec"].population.remove(min_fit_ind)
+					args["num_selected"] = len(args["_ec"].population)
+
 	return
 
 
@@ -70,8 +85,5 @@ def ea_observer2(population, num_generations, num_evaluations, args):
 	sf.write("{},".format(generation_stats["improvement"]))
 	if args["mab"] is not None:
 		sf.write("{}\n".format(args["mab"].n_selections))
-	if args["dynamic_population"]:
-		args["improvement"].pop(0)
-		args["improvement"].append(generation_stats["improvement"])
 
 	return
